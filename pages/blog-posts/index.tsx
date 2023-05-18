3459890b30fd93434b8e7dde7edb03cb6efa582d
cbadour <cbadour@addbloom.com>
2023-05-18 11:40:58 +0300

    add super token and guard routes

--- pages/blog-posts/index.tsx
+++ pages/blog-posts/index.tsx
@@ -1,25 +1,30 @@
 import { AntdListInferencer } from "@refinedev/inferencer/antd";
 import { GetServerSideProps } from "next";
 import { authProvider } from "src/authProvider";
+import { SessionAuth } from "supertokens-auth-react/recipe/session";
 
 export default function BlogPostList() {
-  return <AntdListInferencer />;
+  return (
+    <SessionAuth>
+      <AntdListInferencer />
+    </SessionAuth>
+  )
 }
 
-export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
-  const { authenticated, redirectTo } = await authProvider.check(context);
+// export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+//   const { authenticated, redirectTo } = await authProvider.check(context);
 
-  if (!authenticated) {
-    return {
-      props: {},
-      redirect: {
-        destination: `${redirectTo}?to=${encodeURIComponent("/blog-posts")}`,
-        permanent: false,
-      },
-    };
-  }
+//   if (!authenticated) {
+//     return {
+//       props: {},
+//       redirect: {
+//         destination: `${redirectTo}?to=${encodeURIComponent("/blog-posts")}`,
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  return {
-    props: {},
-  };
-};
+//   return {
+//     props: {},
+//   };
+// };
--- src/authProvider.ts
+++ src/authProvider.ts
@@ -1,8 +1,6 @@
 import { AuthBindings } from "@refinedev/core";
 import nookies from "nookies";
-import supertokensNode from "supertokens-node";
-import { backendConfig } from "./config/backendConfig";
-import Session from 'supertokens-node/recipe/session';
+import { signOut } from "supertokens-web-js/recipe/thirdpartyemailpassword";
 
 const mockUsers = [
   {
@@ -44,73 +42,16 @@ export const authProvider: AuthBindings = {
     };
   },
   logout: async () => {
-    nookies.destroy(null, "auth");
+    await signOut();
     return {
       success: true,
       redirectTo: "/auth",
     };
   },
   check: async (context: any) => {
-
-    // this runs on the backend, so we must call init on supertokens-node SDK
-    supertokensNode.init(backendConfig())
-
-    // this will contain the session object post verification
-    let session;
-
-    try {
-      // getSession will do session verification for us
-      session = await Session.getSession(context.req, context.res, {
-        overrideGlobalClaimValidators: () => {
-          // this makes it so that no custom session claims are checked
-          return []
-        }
-      });
-
-      return {
-        authenticated: true
-      }
-    } catch (err: any) {
-
-      return {
-        authenticated: false,
-        logout: true,
-        redirectTo: "/auth",
-      };
-
-      // if (err.type === Session.Error.TRY_REFRESH_TOKEN) {
-
-      //   // in this case, the session is still valid, only the access token has expired.
-      //   // The refresh token is not sent to this route as it's tied to the /api/auth/session/refresh API paths.
-      //   // So we must send a "signal" to the frontend which will then call the 
-      //   // refresh API and reload the page.
-
-      //   return { props: { fromSupertokens: 'needs-refresh' } }
-      //   // or return {fromSupertokens: 'needs-refresh'} in case of getInitialProps
-      // } else if (err.type === Session.Error.UNAUTHORISED) {
-      //   // in this case, there is no session, or it has been revoked on the backend.
-      //   // either way, sending this response will make the frontend try and refresh
-      //   // which will fail and redirect the user to the login screen.
-      //   return { props: { fromSupertokens: 'needs-refresh' } }
-      // }
-
-      // throw err
-    }
-
-
-    // console.log('checking',)
-    // const cookies = nookies.get(ctx);
-    // if (cookies["auth"]) {
-    //   return {
-    //     authenticated: true,
-    //   };
-    // }
-
-    // return {
-    //   authenticated: false,
-    //   logout: true,
-    //   redirectTo: "/auth",
-    // };
+    return {
+      authenticated: true,
+    };
   },
   getPermissions: async () => {
     const auth = nookies.get()["auth"];
